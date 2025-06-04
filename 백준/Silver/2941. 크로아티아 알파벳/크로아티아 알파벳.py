b3c954384f7c5dcf1da0ab4bdbaf58df899567ca
ctm-sh <105091415+ctm-sh@users.noreply.github.com>
2025-06-04 20:57:40 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,11 +1,7 @@
 a=input()
 d = ['c=','c-','dz=','d-','lj','nj','s=','z=']
-cnt=0
-for j in a:
-    for i in d:        
-        if i in a:
-            a=a.replace(i,' ',1)
-            cnt+=1
-            break
-a=a.replace(' ','')
-print(len(a)+cnt)
+
+for i in d:         
+    a=a.replace(i,' ')
+
+print(len(a))
