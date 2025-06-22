e5cb190e1c00081c2a4d16c47aa0dae05a59fd41
ctm-sh <105091415+ctm-sh@users.noreply.github.com>
2025-06-22 23:54:47 +0900

    [Silver IV] Title: 스택 2, Time: 1152 ms, Memory: 72512 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -9,19 +9,19 @@ for i in range(n):
     if int(order[0])==1:
         queue.append(int(order[1]))
     elif int(order[0])==2:
-        if len(queue)>0:
+        if queue:
             print(queue.pop())
         else:
             print(-1)
     elif int(order[0])==3:
         print(len(queue))
     elif int(order[0])==4:
-        if len(queue)==0:
+        if not queue:
             print(1)
         else:
             print(0)
     elif int(order[0])==5:
-        if len(queue)>0:
+        if queue:
             print(queue[-1])
         else:
             print(-1)
